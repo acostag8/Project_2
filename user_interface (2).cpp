696565f3fcaef4fc6bdfc875916ac9e8ce2fc75c
acostag8 <124711565+acostag8@users.noreply.github.com>
2023-02-14 12:40:24 -0500

    Add files via upload

---
+++ user_interface (2).cpp
@@ -0,0 +1,183 @@
+//=====[Libraries]=============================================================
+
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+#include "user_interface.h"
+
+#include "code.h"
+#include "siren.h"
+#include "smart_home_system.h"
+#include "fire_alarm.h"
+#include "date_and_time.h"
+#include "temperature_sensor.h"
+#include "servo_motor.h"
+#include "gas_sensor.h"
+#include "matrix_keypad.h"
+#include "display.h"
+
+//=====[Declaration of private defines]========================================
+
+#define DISPLAY_REFRESH_TIME_MS 1000
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut incorrectCodeLed(LED3);
+DigitalOut systemBlockedLed(LED2);
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+char codeSequenceFromUserInterface[CODE_NUMBER_OF_KEYS];
+
+//=====[Declaration and initialization of private global variables]============
+
+static bool incorrectCodeState = OFF;
+static bool systemBlockedState = OFF;
+
+static bool codeComplete = false;
+static int numberOfCodeChars = 0;
+static char keyReleased = matrixKeypadUpdate();
+
+//=====[Declarations (prototypes) of private functions]========================
+
+static void userInterfaceMatrixKeypadUpdate();
+static void incorrectCodeIndicatorUpdate();
+static void systemBlockedIndicatorUpdate();
+
+static void userInterfaceDisplayInit();
+static void userInterfaceDisplayUpdate();
+
+//=====[Implementations of public functions]===================================
+
+void userInterfaceInit()
+{
+    incorrectCodeLed = OFF;
+    systemBlockedLed = OFF;
+    matrixKeypadInit( SYSTEM_TIME_INCREMENT_MS );
+    userInterfaceDisplayInit();
+}
+
+void userInterfaceUpdate()
+{
+    userInterfaceMatrixKeypadUpdate();
+    incorrectCodeIndicatorUpdate();
+    systemBlockedIndicatorUpdate();
+    userInterfaceDisplayUpdate();
+}
+
+bool incorrectCodeStateRead()
+{
+    return incorrectCodeState;
+}
+
+void incorrectCodeStateWrite( bool state )
+{
+    incorrectCodeState = state;
+}
+
+bool systemBlockedStateRead()
+{
+    return systemBlockedState;
+}
+
+void systemBlockedStateWrite( bool state )
+{
+    systemBlockedState = state;
+}
+
+bool userInterfaceCodeCompleteRead()
+{
+    return codeComplete;
+}
+
+void userInterfaceCodeCompleteWrite( bool state )
+{
+    codeComplete = state;
+}
+
+//=====[Implementations of private functions]==================================
+
+static void userInterfaceMatrixKeypadUpdate()
+{
+    static int numberOfHashKeyReleased = 0;
+    char keyReleased = matrixKeypadUpdate();
+
+    if( keyReleased != '\0' ) {
+
+        if( sirenStateRead() && !systemBlockedStateRead() ) {
+            if( !incorrectCodeStateRead() ) {
+                codeSequenceFromUserInterface[numberOfCodeChars] = keyReleased;
+                numberOfCodeChars++;
+                if ( numberOfCodeChars >= CODE_NUMBER_OF_KEYS ) {
+                    codeComplete = true;
+                    numberOfCodeChars = 0;
+                }
+            } else {
+                if( keyReleased == '#' ) {
+                    numberOfHashKeyReleased++;
+                    if( numberOfHashKeyReleased >= 2 ) {
+                        numberOfHashKeyReleased = 0;
+                        numberOfCodeChars = 0;
+                        codeComplete = false;
+                        incorrectCodeState = OFF;
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void userInterfaceDisplayInit()
+{
+    displayInit();
+     
+    displayCharPositionWrite ( 0,0 );
+    displayStringWrite( "Code:" );
+    
+}
+
+static void userInterfaceDisplayUpdate()
+{
+    static int accumulatedDisplayTime = 0;
+    displayCharPositionWrite ( 5,0 );
+    displayStringWrite(codeSequenceFromUserInterface);
+
+    if( accumulatedDisplayTime >=
+        DISPLAY_REFRESH_TIME_MS ) {
+
+        accumulatedDisplayTime = 0;     
+    } else {
+        accumulatedDisplayTime =
+            accumulatedDisplayTime + SYSTEM_TIME_INCREMENT_MS;        
+      }
+
+}
+bool gatecode(){
+    if (codeSequenceFromUserInterface == {'1','1','1','1'}) {
+         displayCharPositionWrite (0,1);
+         displayStringWrite ("Correct Code!");
+         gateOpen();
+    }
+    else {
+        displayCharPositionWrite (0,1);
+        displayStringWrite ("Correct Code!");
+    }
+    if (numberOfIncorrectCodes == 3){
+         displayCharPositionWrite (0,1);
+        displayStringWrite ("Locked out!");
+
+    }
+}
+
+static void incorrectCodeIndicatorUpdate()
+{
+    incorrectCodeLed = incorrectCodeStateRead();
+}
+static void systemBlockedIndicatorUpdate()
+{
+    systemBlockedLed = systemBlockedState;
+}
